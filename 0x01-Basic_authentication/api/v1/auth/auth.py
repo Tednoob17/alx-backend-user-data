acdc29d1b33e356a8a8585c4517800d80cf49f01
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:32:46 +0100

    not request ok

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -25,7 +25,7 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
-        if request is None or 'Authorization' 
+        if request is None or 'Authorization' not in request.headers:
             return None
         return request.headers.get('Authorization')
 
