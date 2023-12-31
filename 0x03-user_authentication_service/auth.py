65b63932bd90f1c0bd69bed0d590db34bd445089
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 18:27:59 +0100

    multipe import ok

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 """Authentication module"""
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
+from typing import Union
+
 
 def _hash_password(password: str) -> bytes:
     """Returns a salted, hashed password"""
