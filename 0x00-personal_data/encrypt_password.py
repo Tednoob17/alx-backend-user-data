6d772f9d0ae9b6037b2b42de435e501aaaf5f384
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:15:54 +0100

    check

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,4 +9,5 @@ def hash_password(password: str) -> bytes:
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-
+    """check if password is valid"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
