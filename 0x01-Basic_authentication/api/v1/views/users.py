b38f81514db04d532f76288e64abb7e4ebfcdb3a
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:35:22 +0100

    view all user ok

--- 0x01-Basic_authentication/api/v1/views/users.py
+++ 0x01-Basic_authentication/api/v1/views/users.py
@@ -6,6 +6,15 @@ from flask import abort, jsonify, request
 from models.user import User
 
 
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def view_all_users() -> str:
+    """ GET /api/v1/users
+    Return:
+      - list of all User objects JSON represented
+    """
+    all_users = [user.to_json() for user in User.all()]
+    return jsonify(all_users)
+
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
