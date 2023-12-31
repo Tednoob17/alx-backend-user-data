aa4b293c67cb0e20fae03f1dac8156dd03c1482e
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 18:12:44 +0100

    nrequest auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -78,6 +78,11 @@ def update_password():
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
     try:
-    
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
