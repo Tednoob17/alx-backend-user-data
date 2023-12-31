71559b8da36564096b6fbdc4150402ad8440ca7e
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 18:12:03 +0100

    new upd

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -71,13 +71,7 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
-@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password():
-    """Update the password"""
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
-    try:
+
         AUTH.update_password(reset_token, new_password)
     except ValueError:
         abort(403)
