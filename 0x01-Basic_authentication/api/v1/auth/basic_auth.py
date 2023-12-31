a8abfbd8dc939196a884397e9a82c283478ca299
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:30:35 +0100

    user extr from crd

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,25 +43,6 @@ class BasicAuth(Auth):
         except Exception:
             return (None, None)
 
-    def user_object_from_credentials(self,
-                                     user_email: str,
-                                     user_pwd: str) -> TypeVar('User'):
-        """User Object from Credentials"""
-        if user_email is None or \
-           not isinstance(user_email, str) or \
-           user_pwd is None or \
-           not isinstance(user_pwd, str):
-            return None
-        try:
-            users = User.search({'email': user_email})
-            if not users or len(users) == 0:
-                return None
-            for user in users:
-                if user.is_valid_password(user_pwd):
-                    return user
-            return None
-        except Exception:
-            return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current User"""
