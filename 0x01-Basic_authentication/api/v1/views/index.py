a571f4209575535b7d167e140e6cb2f46ff7ea8c
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:38:14 +0100

    view routes

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
+from flask import jsonify, abort
+from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
-    """
-    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
