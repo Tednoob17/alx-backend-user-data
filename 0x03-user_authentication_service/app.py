987ea8a2fd866fea5ef593cfe2fb4ede3a19d1a3
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 18:08:47 +0100

    request pass

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -8,18 +8,13 @@ app = Flask(__name__)
 AUTH = Auth()
 
 
+@app.route('/')
+def status():
+    """Simple route to test the app"""
+    return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def register():
-    """Register a user"""
-    email = request.form.get('email')
-    password = request.form.get('password')
-    try:
-        AUTH.register_user(email, password)
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
-    return jsonify({"email": email, "message": "user created"}), 200
 
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
