2a35ebdf07e4cc33bba129e953f283aeed121bd7
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:39:11 +0100

    unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,12 +27,7 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    Return:
-      - abort(401)
-    """
-    abort(401)
+
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
