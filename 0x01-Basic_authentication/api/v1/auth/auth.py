5758deb7d2d31c35479ce12e5595c213975b3367
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:31:35 +0100

    class auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,23 +6,6 @@ from flask import Flask, request
 from typing import List, TypeVar
 
 
-class Auth:
-    """Auth class"""
-
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Require auth"""
-        if path is None or excluded_paths is None or excluded_paths == []:
-            return True
-        if path[-1] != '/':
-            path += '/'
-        for i in excluded_paths:
-            if i[-1] == '*':
-                if path.startswith(i[:-1]):
-                    return False
-            elif path == i:
-                return False
-        return True
-
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
         if request is None or 'Authorization' not in request.headers:
