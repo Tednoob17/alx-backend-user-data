0837e6ba213bc8db4dd53cd74bbc9b8d0bbc4a8b
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:38:40 +0100

    strict slashes

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -15,15 +15,6 @@ def status() -> str:
 
 
 @app_views.route('/stats/', strict_slashes=False)
-def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
-    """
-    from models.user import User
-    stats = {}
-    stats['users'] = User.count()
-    return jsonify(stats)
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
