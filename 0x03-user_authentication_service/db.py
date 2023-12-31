b6ad8a4db116d85d8eb8754cc9c6e8ac705a8577
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 18:36:25 +0100

    self union 01

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -25,6 +25,8 @@ class DB:
         """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Save the user to the database"""
