8cab18fe2c9db981da95b6862d9de0e95cb955f6
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 18:36:49 +0100

    add user01

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -28,6 +28,12 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Save the user to the database"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """Find the first user"""
