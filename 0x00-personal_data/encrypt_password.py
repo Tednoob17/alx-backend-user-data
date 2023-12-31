e84620e7b945f2e115159350dee5c91ec497bab5
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:15:32 +0100

    hash password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,3 +2,9 @@
 """Encrypting passwords"""
 import bcrypt
 
+
+def hash_password(password: str) -> bytes:
+    """hash a password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
