e934ab7d881075c5f8f9a873b4f8b7601b14f1a5
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:35:00 +0100

    from user

--- 0x01-Basic_authentication/api/v1/views/users.py
+++ 0x01-Basic_authentication/api/v1/views/users.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
-from api.v1.views import app_views
-from flask import abort, jsonify, request
-from models.user import User
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
