b10f22c15f624d8b702b2ae8d7a086e3d4460bea
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 18:35:05 +0100

    class db ok

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -10,6 +10,9 @@ from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
+class DB:
+    """DB class"""
+
     def __init__(self) -> None:
         """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=False)
