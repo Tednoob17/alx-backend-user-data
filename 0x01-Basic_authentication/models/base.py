a3f083f7dd773c193924aec9699707a2d5ff39fe
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:53:35 +0100

    count cls

--- 0x01-Basic_authentication/models/base.py
+++ 0x01-Basic_authentication/models/base.py
@@ -102,11 +102,6 @@ class Base():
             self.__class__.save_to_file()
 
     @classmethod
-    def count(cls) -> int:
-        """ Count all objects
-        """
-        s_class = cls.__name__
-        return len(DATA[s_class].keys())
 
     @classmethod
     def all(cls) -> Iterable[TypeVar('Base')]:
