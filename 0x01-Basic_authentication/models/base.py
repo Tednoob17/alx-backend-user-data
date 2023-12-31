3f22c9198ff4732e1c9d89500116afa19b932b96
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:50:54 +0100

    class base

--- 0x01-Basic_authentication/models/base.py
+++ 0x01-Basic_authentication/models/base.py
@@ -16,24 +16,6 @@ class Base():
     """ Base class
     """
 
-    def __init__(self, *args: list, **kwargs: dict):
-        """ Initialize a Base instance
-        """
-        s_class = str(self.__class__.__name__)
-        if DATA.get(s_class) is None:
-            DATA[s_class] = {}
-
-        self.id = kwargs.get('id', str(uuid.uuid4()))
-        if kwargs.get('created_at') is not None:
-            self.created_at = datetime.strptime(kwargs.get('created_at'),
-                                                TIMESTAMP_FORMAT)
-        else:
-            self.created_at = datetime.utcnow()
-        if kwargs.get('updated_at') is not None:
-            self.updated_at = datetime.strptime(kwargs.get('updated_at'),
-                                                TIMESTAMP_FORMAT)
-        else:
-            self.updated_at = datetime.utcnow()
 
     def __eq__(self, other: TypeVar('Base')) -> bool:
         """ Equality
