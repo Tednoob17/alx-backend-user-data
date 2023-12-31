9a5ee5ffbcb83bc8ddf8f8c6bf898dd6d63c791d
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:32:08 +0100

    current user

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,6 +23,9 @@ class Auth:
                 return False
         return True
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """Current user"""
-        return None
+    def authorization_header(self, request=None) -> str:
+        """Authorization header"""
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
+
