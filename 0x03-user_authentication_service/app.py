d60468b6b23c506fb3f1ab7b80bccaa484478dce
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 18:11:25 +0100

    reset

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,7 +63,13 @@ def profile():
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token():
     """Get the reset password token"""
-  
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
