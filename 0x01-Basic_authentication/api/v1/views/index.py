70e5d9e8dc0ccc73e1ccc5a015df0bc74549e375
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:38:26 +0100

    def stat

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -6,6 +6,12 @@ from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
