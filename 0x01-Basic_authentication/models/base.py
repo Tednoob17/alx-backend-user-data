73d159cf7e37f11fe08da92049cb49811b8ccdd3
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:50:36 +0100

    timestamp ok

--- 0x01-Basic_authentication/models/base.py
+++ 0x01-Basic_authentication/models/base.py
@@ -8,6 +8,62 @@ import json
 import uuid
 
 
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
+DATA = {}
+
+
+class Base():
+    """ Base class
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initialize a Base instance
+        """
+        s_class = str(self.__class__.__name__)
+        if DATA.get(s_class) is None:
+            DATA[s_class] = {}
+
+        self.id = kwargs.get('id', str(uuid.uuid4()))
+        if kwargs.get('created_at') is not None:
+            self.created_at = datetime.strptime(kwargs.get('created_at'),
+                                                TIMESTAMP_FORMAT)
+        else:
+            self.created_at = datetime.utcnow()
+        if kwargs.get('updated_at') is not None:
+            self.updated_at = datetime.strptime(kwargs.get('updated_at'),
+                                                TIMESTAMP_FORMAT)
+        else:
+            self.updated_at = datetime.utcnow()
+
+    def __eq__(self, other: TypeVar('Base')) -> bool:
+        """ Equality
+        """
+        if type(self) != type(other):
+            return False
+        if not isinstance(self, Base):
+            return False
+        return (self.id == other.id)
+
+    def to_json(self, for_serialization: bool = False) -> dict:
+        """ Convert the object a JSON dictionary
+        """
+        result = {}
+        for key, value in self.__dict__.items():
+            if not for_serialization and key[0] == '_':
+                continue
+            if type(value) is datetime:
+                result[key] = value.strftime(TIMESTAMP_FORMAT)
+            else:
+                result[key] = value
+        return result
+
+    @classmethod
+    def load_from_file(cls):
+        """ Load all objects from file
+        """
+        s_class = cls.__name__
+        file_path = ".db_{}.json".format(s_class)
+        DATA[s_class] = {}
         if not path.exists(file_path):
             return
 
