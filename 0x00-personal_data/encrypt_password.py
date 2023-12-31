73798e44edfb7ef2809b359650a6141906a28919
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:16:16 +0100

    enc check

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,6 +5,7 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """hash a password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
