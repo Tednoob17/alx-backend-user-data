829567ff6e61f1d228f46fe3e44bc18d42600c24
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 18:19:20 +0100

    sessionid

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,4 +11,5 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
 
