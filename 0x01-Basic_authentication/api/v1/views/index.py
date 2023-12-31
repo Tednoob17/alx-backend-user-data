06688e3cecbfa1b5158c466878dca104fedf23a3
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:39:39 +0100

    forbidden ok

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -36,4 +36,9 @@ def unauthorized() -> str:
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - abort(403)
+    """
+    abort(403)
