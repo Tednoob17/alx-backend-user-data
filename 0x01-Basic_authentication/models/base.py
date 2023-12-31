5c0c72c24c96df752aa7f52fc78113e32effe559
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 17:54:08 +0100

    get

--- 0x01-Basic_authentication/models/base.py
+++ 0x01-Basic_authentication/models/base.py
@@ -115,11 +115,6 @@ class Base():
         return cls.search()
 
     @classmethod
-    def get(cls, id: str) -> TypeVar('Base'):
-        """ Return one object by ID
-        """
-        s_class = cls.__name__
-        return DATA[s_class].get(id)
 
     @classmethod
     def search(cls, attributes: dict = {}) -> List[TypeVar('Base')]:
